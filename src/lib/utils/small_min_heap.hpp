5aba8757bbb75c4cebd3635d0dd1d4ee5a0eb945
Niklas Mohrin <dev@niklasmohrin.de>
2023-08-02 21:32:30 +0200

    Add `SmallMinHeap`

---
+++ src/lib/utils/small_min_heap.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <array>
+#include <functional>
+#include <ranges>
+
+#include "assert.hpp"
+
+namespace hyrise {
+
+template <uint8_t max_size, typename T, typename Compare = std::less<T>>
+class SmallMinHeap {
+ public:
+  template <typename R>
+    requires std::ranges::input_range<R> && std::ranges::sized_range<R>
+  explicit SmallMinHeap(R&& initial_elements, Compare&& init_compare = {}) : _compare(std::move(init_compare)) {
+    const auto size = std::ranges::size(initial_elements);
+    Assert(size <= max_size, "SmallMinHeap got more than max_size initial elements");
+    _size = static_cast<uint8_t>(size);
+    std::ranges::move(initial_elements, _elements.begin());
+    std::ranges::sort(std::span(_elements).subspan(0, _size), _compare);
+  }
+
+  uint8_t size() const {
+    return _size;
+  }
+
+  void push(T element) {
+    DebugAssert(size() < max_size, "Pushed into already full SmallMinHeap");
+
+    const auto insertion_point =
+        std::ranges::find_if(_elements.begin(), _elements.begin() + _size,
+                             [&](const auto& contained) { return _compare(element, contained); });
+    std::ranges::move_backward(insertion_point, _elements.begin() + _size, _elements.begin() + _size + 1);
+    *insertion_point = std::move(element);
+    ++_size;
+  }
+
+  const T& top() const {
+    return _elements.front();
+  }
+
+  T pop() {
+    DebugAssert(size() > 0, "Popped from empty SmallMinHeap");
+    auto result = std::move(_elements.front());
+    std::ranges::move(_elements.begin() + 1, _elements.begin() + _size, _elements.begin());
+    --_size;
+    return result;
+  }
+
+ private:
+  std::array<T, max_size> _elements;
+  Compare _compare;
+  uint8_t _size;
+};
+
+}  // namespace hyrise
